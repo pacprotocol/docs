f3ba53fca451c2927dcababc4706c315034aa30b
YanDevDe <1526048+YanDevDe@users.noreply.github.com>
2021-07-11 20:52:08 +0200

    Fix Config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -176,14 +176,14 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/pacprotocol/docs/edit/master/website/',
+            'https://github.com/pacprotocol/docs/edit/master/',
         },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/pacprotocol/docs/edit/master/website/blog/',
-        },
+        //blog: {
+        //  showReadingTime: true,
+        //  // Please change this to your repo.
+        //  editUrl:
+        //    'https://github.com/pacprotocol/docs/edit/master/website/blog/',
+        //},
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
